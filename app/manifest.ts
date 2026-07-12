2c033bc7fa079456efb09018f95b29b086a6bf7c
Somojit Arambam <somoarambam123@gmail.com>
2026-07-12 23:39:55 +0530

    feat: update website

--- app/manifest.ts
+++ app/manifest.ts
@@ -14,13 +14,13 @@ export default function manifest(): MetadataRoute.Manifest {
         icons: [
 
             {
-                src: '/icon-192x192.png?v=3',
+                src: '/icon-192x192.png?v=4',
                 sizes: '192x192',
                 type: 'image/png',
                 purpose: 'maskable',
             },
             {
-                src: '/icon-512x512.png?v=3',
+                src: '/icon-512x512.png?v=4',
                 sizes: '512x512',
                 type: 'image/png',
                 purpose: 'any',
